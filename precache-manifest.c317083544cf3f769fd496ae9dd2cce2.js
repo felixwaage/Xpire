8845273bbf754c1f01f5c8d7938c8b6975eaeacb
Milena <milena.zahn@gmx.de>
2020-06-05 15:55:39 +0200

    Updates

---
+++ precache-manifest.c317083544cf3f769fd496ae9dd2cce2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e81e007af441d871a608d053534ab6c",
+    "url": "/Xpire/index.html"
+  },
+  {
+    "revision": "b0e5285fbf837f4b88da",
+    "url": "/Xpire/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e5b0387373ddcb91836d",
+    "url": "/Xpire/static/js/2.73b32bf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Xpire/static/js/2.73b32bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e5285fbf837f4b88da",
+    "url": "/Xpire/static/js/main.7b213ca9.chunk.js"
+  },
+  {
+    "revision": "0a520e51c0c0bc144f5e",
+    "url": "/Xpire/static/js/runtime-main.ee034db2.js"
+  },
+  {
+    "revision": "3fb9c233464a1a7a53baf97965f0fdfb",
+    "url": "/Xpire/static/media/logo.3fb9c233.svg"
+  }
+]);
