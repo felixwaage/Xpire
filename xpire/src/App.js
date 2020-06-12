2595a5b69dc39e48a4b98fd06324834f938542ff
Fabio Westphal <torheld@gmx.de>
2020-06-12 17:46:41 +0200

    dexie test fixed

--- xpire/src/App.js
+++ xpire/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
       <AppHeader />
       <ProductsList />
+      <Idb></Idb>
     </div>
   );
 }
--- xpire/src/Idb.js
+++ xpire/src/Idb.js
@@ -4,35 +4,47 @@ import Dexie from 'dexie'
 function Idb() {
 
     const [allProducts, setProducts] = useState([]);
-    const db = new Dexie();
+    const db = new Dexie("Xpire");
 
     const handler = async () => {
+        console.log("async handler")
         db.version(1).stores({
             products: "++id,name,expireDate"
+        });
+        db.open().catch(function (err) {
+            console.error (err.stack || err);
         });
         await db.products.add({
             name: 'Kartoffel',
             expireDate: '23.08.3021'
         });
-        setProducts(db.products);
         await db.products.toArray().then(function (arr) {
-            console.log(arr);
             setProducts(arr);
         });
+    }
+    const addProduct = (productArray) => {
+        setProducts(allProducts.concat(productArray));
     }
 
     const deleteAProduct = () => {
         db.products.where('id').equals(1);
     }
+    const clearTable = async () => {
+        await db.open().catch(function (err) {
+            console.error (err.stack || err);
+        });
+        await db.table("products").clear();
+        db.close();
+    }
 
     return (
         <div>
             <button onClick={handler}>Test</button>
-            <button onClick={deleteAProduct}>Delete</button>
+            <button onClick={clearTable}>Clear</button>
             <ul>
                 {allProducts.map((product) =>
                     <li>{product.name}</li>
-                )}
+                ) }
             </ul>
         </div>
     )
